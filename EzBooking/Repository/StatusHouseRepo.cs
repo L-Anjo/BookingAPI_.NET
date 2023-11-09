67c4f1965be5ae9475c922e95ce090ed6c298e2b
L-Anjo <a23528@alunos.ipca.pt>
2023-11-09 19:24:33 +0000

    mais um

---
+++ EzBooking/Repository/StatusHouseRepo.cs
@@ -0,0 +1,21 @@
+﻿using EzBooking.Data;
+using EzBooking.Models;
+
+namespace EzBooking.Repository
+{
+    public class StatusHouseRepo
+    {
+        private readonly DataContext _context;
+
+        public StatusHouseRepo(DataContext context)
+        {
+            _context = context;
+        }
+
+        public StatusHouse GetStatusHouseById(int id)
+        {
+            return _context.StatusHouses
+                .FirstOrDefault(p => p.id == id);
+        }
+    }
+}
