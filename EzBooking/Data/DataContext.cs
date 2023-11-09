325e1ae26d20e1312ea818511715d9489c124d17
Pedro Martins <a23527@alunos.ipca.pt>
2023-11-09 19:23:27 +0000

    dff

--- EzBooking/Data/DataContext.cs
+++ EzBooking/Data/DataContext.cs
@@ -9,6 +9,7 @@ namespace EzBooking.Data
         {
             
         }
+
         public DbSet<House> Houses{ get; set; }
         public DbSet<PostalCode> PostalCodes { get; set; }
         public DbSet<StatusHouse> StatusHouses { get; set; }
